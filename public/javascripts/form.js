716b25eef235e6af390df2bea665f426f4b487bd
Saffr3n-TOP <saffr3n.reserve@gmail.com>
2024-02-08 15:31:39 +0300

    Implement register POST route

---
+++ public/javascripts/form.js
@@ -0,0 +1,3 @@
+if (document.querySelector('#hint')) {
+  hint.parentElement.focus();
+}
--- src/controller.ts
+++ src/controller.ts
@@ -1,9 +1,9 @@
-import { Request as Req, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { body, validationResult } from 'express-validator';
+import bcrypt from 'bcryptjs';
 import createError from 'http-errors';
-import Message from './models/message';
 import User from './models/user';
-
-type Request = Req & { user?: User };
+import Message from './models/message';
 
 export async function index(req: Request, res: Response, next: NextFunction) {
   const messages = await Message.find()
@@ -37,9 +37,57 @@ export function registerGet(req: Request, res: Response, next: NextFunction) {
   res.status(200).render('register', { title: 'Sign Up' });
 }
 
-export function registerPost(req: Request, res: Response, next: NextFunction) {
-  res.send('NOT IMPLEMENTED: Register POST route');
-}
+export const registerPost = [
+  body('email', 'Email is invalid').trim().notEmpty().bail().isEmail().bail(),
+  body('password', 'Password is too short').trim().isLength({ min: 8 }).bail(),
+  body('confirm', "Passwords don't match")
+    .trim()
+    .custom((value, { req }) => value === req.body.password)
+    .bail(),
+  body('firstName', 'First name is required').trim().notEmpty().bail(),
+  body('lastName', 'Last name is required').trim().notEmpty(),
+
+  async function (req: Request, res: Response, next: NextFunction) {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).render('register', {
+        title: 'Sign Up',
+        formData: req.body,
+        error: errors.array()[0]
+      });
+    }
+
+    const hash = await bcrypt.hash(req.body.password, 12).catch(() => {});
+
+    if (!hash) {
+      const err = createError(500, 'Server Error');
+      return next(err);
+    }
+
+    const user = new User({
+      email: req.body.email,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      hash
+    });
+
+    const isSaved = !!(await user.save().catch(() => {}));
+
+    if (!isSaved) {
+      const err = createError(500, 'No Database Response');
+      return next(err);
+    }
+
+    req.login(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+
+      res.redirect('/');
+    });
+  }
+];
 
 export function logout(req: Request, res: Response, next: NextFunction) {
   res.send('NOT IMPLEMENTED: Logout route');
